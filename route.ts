a5a3514180919ce4848abe3681ea5a17c2887ed7
mehraan09 <aakshh5kaa@gmail.com>
2025-08-23 10:45:32 +0530

    Add files via upload

---
+++ route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient, Author } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+export async function POST(req: Request) {
+  const body = await req.json()
+  const { message } = body
+
+  console.log("message : " ,  message)
+
+  await prisma.ifshe.create({
+    data: {
+      message : message,
+    },
+  })
+
+  return new Response("Reason added", { status: 201 })
+}
